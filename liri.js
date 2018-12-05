f0c1c8e3c22fdbec9d0a7b27aa9e11ad66b0260e
Wesley Fonseca <wfonseca18@gmail.com>
2018-12-05 12:47:01 -0500

    Adding code to liri.js

--- liri.js
+++ liri.js
@@ -1,72 +1,45 @@
 
 require('dotenv').config();
 
-// const spotify = new Spotify(keys.spotify);
+const spotify = new Spotify(keys.spotify);
+var command = process.argv[2], argument = process.argv[3];
 
-
-/*
-1. `node liri.js concert-this <artist/band name here>`
-
-   * This will search the Bands in Town Artist Events API (`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`) for an artist and render the following information about each event to the terminal:
-
-     * Name of the venue
-
-     * Venue location
-
-2. `node liri.js spotify-this-song '<song name here>'`
-
-   * This will show the following information about the song in your terminal/bash window
-
-     * Artist(s)
-
-     * The song's name
-
-     * A preview link of the song from Spotify
-
-     * The album that the song is from
-
-   * If no song is provided then your program will default to "What's My Age Again" by blink-182.
-
-   * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
-
-   * The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a **client id** and **client secret**:
-
-   * Step One: Visit <https://developer.spotify.com/my-applications/#!/>
-
-   * Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-
-   * Step Three: Once logged in, navigate to <https://developer.spotify.com/my-applications/#!/applications/create> to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-
-   * Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the [node-spotify-api package](https://www.npmjs.com/package/node-spotify-api).
-
-3. `node liri.js movie-this '<movie name here>'`
-
-   * This will output the following information to your terminal/bash window:
-
-     ```
-       * Title of the movie.
-       * Year the movie came out.
-       * IMDB Rating of the movie.
-       * Rotten Tomatoes Rating of the movie.
-       * Country where the movie was produced.
-       * Language of the movie.
-       * Plot of the movie.
-       * Actors in the movie.
-     ```
-
-   * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-     * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-
-     * It's on Netflix!
-
-   * You'll use the request package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use `trilogy`.
-
-4. `node liri.js do-what-it-says`
-
-   * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-     * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-
-     * Edit the text in random.txt to test out the feature for movie-this and my-tweets
-*/
+switch (command.toLowerCase) {
+    case 'concert-this':
+        console.log(`API https://rest.bandsintown.com/artists/${argument}/events?app_id=codingbootcamp`);
+        console.log('Name of the venue');
+        console.log('Venue location');
+        break;
+    case 'spotify-this-song':
+        console.log(`API call to: https://rest.bandsintown.com/artists/${argument}/events?app_id=codingbootcamp`);
+        if (argument) {
+            console.log('Artist(s)');
+            console.log(`The song's name`);
+            console.log('A preview link of the song from Spotify');
+            console.log('The album that the song is from');
+        } else {
+            console.log(`default to "What's My Age Again" by blink-182`);
+        }
+        break;
+    case 'movie-this':
+        console.log(`http://www.omdbapi.com/?i=${argument}&apikey=trilogy`);
+        if (argument) {
+            console.log('Title of the movie');
+            console.log('Year the movie came out');
+            console.log('IMDB Rating of the movie');
+            console.log('Rotten Tomatoes Rating of the movie');
+            console.log('Country where the movie was produced');
+            console.log('Language of the movie');
+            console.log('Plot of the movie');
+            console.log('Actors in the movie');
+        } else {
+            console.log(`output data for the movie 'Mr. Nobody.'`);
+        }
+        break;
+    case 'do-what-it-says':
+        console.log('run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`');
+        console.log('Edit the text in random.txt to test out the feature for movie-this and my-tweets');
+        break;
+    default:
+        break;
+}
