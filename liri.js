99015fbd32a1e26fd227e69b8dd41bd6651d8a14
Wesley Fonseca <wfonseca18@gmail.com>
2018-12-06 12:45:44 -0500

    Filling out the switch cases

--- liri.js
+++ liri.js
@@ -1,45 +1,81 @@
 
+// Imports
 require('dotenv').config();
+const keys = require('./keys.js');
+const Spotify = require('node-spotify-api');
+const request = require('request');
 
+// Declaring global variables
 const spotify = new Spotify(keys.spotify);
-var command = process.argv[2], argument = process.argv[3];
+var command = process.argv[2], argument = process.argv[3], queryUrl, body;
 
-switch (command.toLowerCase) {
+
+switch (command.toLowerCase()) {
     case 'concert-this':
-        console.log(`API https://rest.bandsintown.com/artists/${argument}/events?app_id=codingbootcamp`);
-        console.log('Name of the venue');
-        console.log('Venue location');
+        queryUrl = `https://rest.bandsintown.com/artists/${argument}/events?app_id=codingbootcamp`;
+        request(queryUrl, function (error, response, body) {
+            // If the request is successful
+            if (!error && response.statusCode === 200) {
+                // Parsing for dot walking.
+                body = JSON.parse(body);
+                console.log(`Venue Name: ${body[0].venue.name}`);
+                console.log(`Venue Location: ${body[0].venue.city}, ${body[0].venue.country}`);
+            } else {
+                console.log('Error occurred: ' + error);
+            }
+        });
         break;
     case 'spotify-this-song':
-        console.log(`API call to: https://rest.bandsintown.com/artists/${argument}/events?app_id=codingbootcamp`);
+        var artists = '', query;
         if (argument) {
-            console.log('Artist(s)');
-            console.log(`The song's name`);
-            console.log('A preview link of the song from Spotify');
-            console.log('The album that the song is from');
+            query = { type: 'track', query: argument };
         } else {
-            console.log(`default to "What's My Age Again" by blink-182`);
+            query = { type: 'track', query: `What's My Age Again` };
         }
+        spotify.search(query, function (err, data) {
+            if (err) {
+                return console.log('Error 2 occurred: ' + err);
+            }
+            body = data.tracks.items[0];
+            body.artists.forEach(artist => {
+                artists += artist.name;
+            });
+            console.log(`Artist(s): ${artists}`);
+            console.log(`Song Name: ${body.name}`);
+            console.log(`Preview Link: ${body.external_urls.spotify}`);
+            console.log(`Album: ${body.album.name}`);
+        });
         break;
     case 'movie-this':
-        console.log(`http://www.omdbapi.com/?i=${argument}&apikey=trilogy`);
-        if (argument) {
-            console.log('Title of the movie');
-            console.log('Year the movie came out');
-            console.log('IMDB Rating of the movie');
-            console.log('Rotten Tomatoes Rating of the movie');
-            console.log('Country where the movie was produced');
-            console.log('Language of the movie');
-            console.log('Plot of the movie');
-            console.log('Actors in the movie');
-        } else {
-            console.log(`output data for the movie 'Mr. Nobody.'`);
+        if(argument){
+            queryUrl = `http://www.omdbapi.com/?t=${argument}&apikey=trilogy`;
+        }else{
+            queryUrl = `http://www.omdbapi.com/?i=tt0485947&apikey=trilogy`;
         }
+        
+        request(queryUrl, function (error, response, body) {
+            // If the request is successful
+            if (!error && response.statusCode === 200) {
+                // Parsing for dot walking.
+                body = JSON.parse(body);
+                console.log(`Title: ${body.Title}`);
+                console.log(`Year: ${body.Year}`);
+                console.log(`IMDB Rating: ${body.Ratings[0].Value}`);
+                console.log(`Rotten Tomatoes Rating: ${body.Ratings[1].Value}`);
+                console.log(`Country: ${body.Country}`);
+                console.log(`Language: ${body.Language}`);
+                console.log(`Plot: ${body.Plot}`);
+                console.log(`Actors: ${body.Actors}`);
+            } else {
+                console.log('Error occurred: ' + error);
+            }
+        });
         break;
     case 'do-what-it-says':
         console.log('run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`');
         console.log('Edit the text in random.txt to test out the feature for movie-this and my-tweets');
         break;
     default:
+        console.log('Invalid command!');
         break;
 }
